66d288c066979a2b1337af63db6e8a982061d3e1
syafzid <18052270@myrp.edu.sg>
2020-06-03 20:20:54 +0800

    Fixed a bug where web view is not loading the url

--- app/src/main/java/com/example/pd4thapp/ThirdActivity.java
+++ app/src/main/java/com/example/pd4thapp/ThirdActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -18,9 +19,14 @@ public class ThirdActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String link = intent.getStringExtra("url");
 
+        StringBuilder sb = new StringBuilder(link);
+        sb.insert(4, "s");
+        String editedLink = sb.toString();
+        Log.i("ThirdActivity", editedLink);
+
         webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl(link);
+        webView.loadUrl(editedLink);
     }
 }
